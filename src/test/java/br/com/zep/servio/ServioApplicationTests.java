2fc94d21f8c620bc2144186ff27e1161eb635475
zep <jososephbem100@gmail.com>
2026-09-23 19:47:22 -0300

    Parte A: banco Postgres real nos testes de integração
    ServioApplicationTests agora sobe com @ActiveProfiles("test") e
    @Import(TestcontainersConfig.class), como as demais suítes de segurança,
    em vez do banco em memória.

--- src/test/java/br/com/zep/servio/ServioApplicationTests.java
+++ src/test/java/br/com/zep/servio/ServioApplicationTests.java
@@ -1,9 +1,14 @@
 package br.com.zep.servio;
 
+import br.com.zep.servio.seguranca.TestcontainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfig.class)
 class ServioApplicationTests {
 
 	@Test
